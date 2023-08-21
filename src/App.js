3222b366f8879aa5b8ee9c84207863a2a13ef4c1
jinjoo-jung <wjdwlswn23@naver.com>
2023-08-21 17:22:36 +0900

    first react

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+
+export default class App extends Component {
+  render() {
+    return (
+      <main className="main-container">
+        <h1>예산 계산기</h1>
+
+        <div
+          style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
+        >
+          <ExpenseForm />
+        </div>
+
+        <div
+          style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
+        >
+          <ExpenseList />
+        </div>
+
+        <div
+          style={{ display: "flex", justifyContent: "end", marginTop: "1rem" }}
+        >
+          <p style={{ fontSize: "2rem" }}>
+            총지출:
+            <span>원</span>
+          </p>
+        </div>
+      </main>
+    );
+  }
+}
