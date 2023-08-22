70b01a680887906560279c950fb261e981d433d5
jinjoo-jung <wjdwlswn23@naver.com>
2023-08-22 22:59:25 +0900

    설명 추가

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./ExpenseList.css";
 import ExpenseItem from "./ExpenseItem";
 
+// 클래스 -> 함수로 변경
 const ExpenseList = ({ expenses, handleDelete }) => {
   return (
     <>
