527cd5460871e31bbf64206c7b11b7a935c334e7
jinjoo-jung <wjdwlswn23@naver.com>
2023-08-22 17:12:16 +0900

    가계부 코드 작성 완성

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,22 +2,24 @@ import React, { Component } from "react";
 import "./ExpenseList.css";
 import ExpenseItem from "./ExpenseItem";
 
-
-const ExpenseList = (expenses, handleDelete) => {
-    return (
-      <>
-        <ul className="list">
-          {expense.map(expenses => {
-            return <ExpenseItem 
-            key={expense.id}
-            expense={expenses} 
-            handleDlete={handleDelete}
-            >;
-          })}
-        </ul>
-        <button className="btn">목록 지우기</button>
-
+const ExpenseList = ({ expenses, handleDelete }) => {
+  return (
+    <>
+      <ul className="list">
+        {expenses.map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              expense={expense}
+              handleDlete={handleDelete}
+            />
           );
-  }
+        })}
+      </ul>
+
+      <button className="btn">목록 지우기</button>
+    </>
+  );
+};
 
 export default ExpenseList;
